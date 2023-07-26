8cf3eca6c66e277bf9de4d2fb072d171aa0a92dd
Mohit <rnippanikar7@gmail.com>
2023-07-27 01:49:58 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -7,14 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose
-  .connect(
-    process.env.MONGODB_URI ||
-      "mongodb+srv://rnippanikar7:mhtnipp77@cluster0.jlxdzt8.mongodb.net/test"
-  )
-  .then(() => {
-    app.listen(process.env.PORT || 3000, () => {
-      console.log("Server running on 3000");
-    });
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("Server running on 3000");
   });
+});
 app.use("/notes", require("./routes/noteR.js"));
